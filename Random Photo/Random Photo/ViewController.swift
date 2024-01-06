614dce8fc134e0119e4ce5df54b13c85fa4a2436
Grisha08 <155233532+Grisha08@users.noreply.github.com>
2024-01-06 14:58:57 +0200

    Add files via upload

---
+++ Random Photo/Random Photo/ViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  Random Photo
+//
+//  Created by Grisha Borodavka on 17/11/2023.
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    private let imageView: UIImageView = {
+    let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.backgroundColor = .white
+    return imageView
+    }()
+    
+    private let button: UIButton = {
+    let button = UIButton()
+        button.setTitle("Random Photo", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.backgroundColor = .white
+    return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .gray
+        view.addSubview(button)
+        button.addTarget(self,action:#selector (didTapButton),for: .touchUpInside)
+                         
+        view.addSubview(imageView)
+        imageView.frame = CGRect(x:
+      0, y: 0, width: 300, height:
+       300)
+        imageView.center = view.center
+        getRandomPhoto()
+        
+    }
+    @objc func didTapButton() {
+        getRandomPhoto()
+        
+    }
+    override func viewDidLayoutSubviews(){
+        super.viewDidLayoutSubviews()
+        button.frame = CGRect(x:30,
+                              y: view.frame.size.height-150-view.safeAreaInsets.bottom,
+                              width: view.frame.size.width-60,
+                              height:55)
+    }
+    func getRandomPhoto() {
+        let urlString = "https://source.unsplash.com/random/600x600"
+        let url = URL(string: urlString)!
+        guard let data = try? Data(contentsOf: url) else {
+            return
+        }
+        imageView.image = UIImage(data: data)
+    }
+    
+}
+
